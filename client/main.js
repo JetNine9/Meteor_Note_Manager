dea49b2b5533fa5ce77195129b701d9a48e2babe
Jethro Talavera <jethro.talavera1@gmail.com>
2021-07-07 14:24:31 -0400

    added materialize

--- client/main.js
+++ client/main.js
@@ -1,2 +1,13 @@
 import { Template } from 'meteor/templating';
 import './main.html';
+import m from 'materialize-css'
+import 'materialize-css/dist/css/materialize.css'
+
+
+Template.body.helpers({
+    notes:[
+        {text: 'My note 1'},
+        {text: 'My note 2'},
+        {text: 'My note 3'},
+    ]
+})
